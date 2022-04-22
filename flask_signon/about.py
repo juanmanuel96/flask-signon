c08ce983fdde908aa46936f36a7d43bceaa78ae2
juanmanuel96 <juanmapr913@gmail.com>
2022-04-21 20:22:48 -0400

    Updated version number

--- flask_signon/about.py
+++ flask_signon/about.py
@@ -1,7 +1,7 @@
 __title__ = 'Flask-Signon'
 __description__ = 'A Flask extension for managing authentication and permissions.'
 __url__ = 'https://github.com/juanmanuel96/flask-signon'
-__version_info__ = ('1', '0', '0')
+__version_info__ = ('1', '0', '1')
 __version__ = '.'.join(__version_info__)
 __author__ = 'Juan Vazquez'
 __author_email__ = 'juanmapr913@gmail.com'
